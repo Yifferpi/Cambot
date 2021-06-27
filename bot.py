4a86c26ed8ddf27467af71a7a39dbab153232ab1
Yifferpi <theotheen.bm@gmail.com>
2021-06-27 22:23:04 +0200

    added access control via user filter

--- bot.py
+++ bot.py
@@ -2,11 +2,18 @@
 
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from telegram.ext import Filters
 from picamera import PiCamera
+from time import sleep
 import logging
 
 
 TOKEN="***REMOVED***"
+USERS=["***REMOVED***", "***REMOVED***"]
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.led = False
 
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
@@ -22,18 +29,15 @@ def start(update, context):
             text="I'm a bot, please talk to me!")
 
 def pic(update, context):
-    camera = PiCamera()
-    camera.resolution = (1024, 768)
     camera.start_preview()
     sleep(2) # Camera warm-up time
     camera.capture('foo.jpg')
     context.bot.send_photo(
             chat_id=update.effective_chat.id,
-            photo='foo.jpg')
-
+            photo=open('foo.jpg', 'rb'))
 
 start_handler = CommandHandler('start', start)
-pic_handler = CommandHandler('pic', pic)
+pic_handler = CommandHandler('pic', pic, Filters.user(username=USERS))
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(pic_handler)
 
