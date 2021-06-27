e1d0eeb055f73d55fbae44c2ceb664cc2e282fec
Yifferpi <theotheen.bm@gmail.com>
2021-06-27 21:36:19 +0200

    added first version of image capture

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from picamera import PiCamera
 import logging
 
 
 TOKEN="***REMOVED***"
 
-updater = Updater(token='TOKEN', use_context=True)
+updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 logging.basicConfig(
@@ -20,9 +21,21 @@ def start(update, context):
             chat_id=update.effective_chat.id, 
             text="I'm a bot, please talk to me!")
 
+def pic(update, context):
+    camera = PiCamera()
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    sleep(2) # Camera warm-up time
+    camera.capture('foo.jpg')
+    context.bot.send_photo(
+            chat_id=update.effective_chat.id,
+            photo='foo.jpg')
+
 
 start_handler = CommandHandler('start', start)
+pic_handler = CommandHandler('pic', pic)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(pic_handler)
 
 updater.start_polling()
 
