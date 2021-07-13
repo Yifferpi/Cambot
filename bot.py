40a9408e6c16e34993a962371ee12408aaac1d71
Yifferpi <theotheen.bm@gmail.com>
2021-07-13 08:25:48 +0200

    added registering of commands

--- bot.py
+++ bot.py
@@ -3,13 +3,14 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import Filters
+from telegram import BotCommand
 from picamera import PiCamera
 from time import sleep
 import logging
 
-
 TOKEN="***REMOVED***"
 USERS=["***REMOVED***", "***REMOVED***"]
+PATH="***REMOVED***/"
 
 camera = PiCamera()
 camera.resolution = (1024, 768)
@@ -23,23 +24,31 @@ logging.basicConfig(
         level=logging.INFO)
 
 
-def start(update, context):
+def about(update, context):
     context.bot.send_message(
             chat_id=update.effective_chat.id, 
-            text="I'm a bot, please talk to me!")
+            text="Hi! I'm the Cambot.\n\
+            I take pictures for you.")
 
 def pic(update, context):
     camera.start_preview()
     sleep(2) # Camera warm-up time
-    camera.capture('foo.jpg')
+    camera.capture(PATH+'foo.jpg')
     context.bot.send_photo(
             chat_id=update.effective_chat.id,
-            photo=open('foo.jpg', 'rb'))
+            photo=open(PATH+'foo.jpg', 'rb'))
+#collect all images in a folder and timestamp them.
+#send message if too many pictures have been taken
 
-start_handler = CommandHandler('start', start)
+start_handler = CommandHandler('about', about)
 pic_handler = CommandHandler('pic', pic, Filters.user(username=USERS))
+commands = [ \
+        BotCommand('about', "Just a test command"), \
+        BotCommand('pic', "Take a picture")]
+
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(pic_handler)
+dispatcher.bot.set_my_commands(commands)
 
 updater.start_polling()
 
